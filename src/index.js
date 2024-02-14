99c71430fda7ed9ca0bf2f140d1e7dc44fd39b0b
David <Marin9737@gmail.com>
2024-02-13 23:10:29 -0500

    ramen rater complete

--- src/index.js
+++ src/index.js
@@ -1,29 +1,71 @@
-// index.js
+ 
+ const displayRamens = () => {
+  fetch("http://localhost:3000/ramens")
+  .then(res => res.json())
+  .then((ramenData) => {
+    ramenData.forEach(displayRamenImage)
 
-// Callbacks
-const handleClick = (ramen) => {
-  // Add code
-};
+  })};
+
+  const displayRamenImage = (ramen) => {
+    const ramenMenu = document.querySelector('#ramen-menu')
+    const ramenImages = document.createElement('img')
+    ramenImages.src = ramen.image
+  
+    ramenMenu.append(ramenImages)
+    ramenImages.addEventListener("click", () => {
+      handleClick(ramen)
+    })
+  }
+
+  const handleClick = (ramens) => {
+    // Add code
+    const detailImage = document.querySelector('.detail-image')
+    const name = document.querySelector('.name')
+    const restaurant = document.querySelector('.restaurant')
+    const ratingDisplay = document.getElementById('rating-display')
+    const commentDisplay = document.getElementById('comment-display')
+  detailImage.src = ramens.image
+  name.textContent = ramens.name
+  restaurant.textContent = ramens.restaurant
+  ratingDisplay.textContent = ramens.rating
+  commentDisplay.textContent = ramens.comment
+  }
+
 
 const addSubmitListener = () => {
   // Add code
+  const newRamen = document.querySelector('#new-ramen')
+
+  newRamen.addEventListener("submit", (e) => {
+
+    e.preventDefault()
+    
+    const newName = e.target['new-name'].value
+    const newRestaurant = e.target['new-restaurant'].value
+    const newImage = e.target['new-image'].value
+    const newRating = e.target['new-rating'].value
+    const newComment = e.target['new-comment'].value
+
+    const form = {
+      "name": newName,
+      "restaurant": newRestaurant,
+      "image": newImage,
+      "rating": newRating,
+      "comment": newComment,
+    }
+    displayRamenImage(form)
+    e.target.reset()
+    
+  })
+
 }
 
-const displayRamens = () => {
-  // Add code
-};
-
 const main = () => {
-  // Invoke displayRamens here
-  // Invoke addSubmitListener here
+  displayRamens()
+  addSubmitListener()
+
 }
 
 main()
 
-// Export functions for testing
-export {
-  displayRamens,
-  addSubmitListener,
-  handleClick,
-  main,
-};
